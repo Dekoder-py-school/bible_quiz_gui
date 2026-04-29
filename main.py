3871605dc5f2e3f2528f2766f28d2694a0754e7f
Dekoder-py <kyle@codingcorner.dev>
2026-04-30 11:49:27 +1200

    Create window with title

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from bible-quiz-gui!")
+import tkinter as tk
+from tkinter import ttk
+
+
+class App:
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title("Bible Quiz")
+
+    def run(self):
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.run()
