6be377975b583e65a304da0a1990f034d0ec333e
Dekoder-py <kyle@codingcorner.dev>
2026-05-08 10:43:06 +1200

    Set up a gridbaglayout to place components correctly.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
 package src;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.*;
 
 class Main {
 
     public static void main(String[] args) {
         JFrame frame = initFrame("Bible Quiz!");
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
 
-        frame.getContentPane().setBackground(Colors.BASE);
+        JLabel questionLabel = new JLabel("QUESTION GOES HERE");
+
+        JTextField answerField = new JTextField(10);
+        JButton markButton = new JButton("Check");
+
+        // ensure the LAF paints the custom background color
+        markButton.setBackground(Colors.MAUVE);
+        markButton.setOpaque(true);
+        markButton.setBorderPainted(false);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(questionLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(answerField, gbc);
+
+        gbc.gridx = 1;
+        panel.add(markButton, gbc);
+
+        frame.add(panel);
+        panel.setBackground(Colors.BASE);
 
         frame.setVisible(true);
     }
