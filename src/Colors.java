610f493cdc3ee255d8747ef3a03e22f0df6d2d57
Dekoder-py <kyle@codingcorner.dev>
2026-05-07 13:25:28 +1200

    Update Colors.java
    Added text color and accent color

--- src/Colors.java
+++ src/Colors.java
@@ -5,4 +5,6 @@ import java.awt.Color;
 public class Colors {
 
     public static Color BASE = new Color(0x1e1e2e);
+    public static Color TEXT = new Color(0xcdd6f4);
+    public static Color MAUVE = new Color(0xcba6f7);
 }
