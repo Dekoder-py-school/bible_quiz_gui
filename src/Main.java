08a1187ea737812052f59b395286e1b0fc4b3ad0
Dekoder-py <kyle@codingcorner.dev>
2026-05-11 09:46:03 +1200

    Seperate stuff into functions Added start screen

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ package src;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+
 import javax.swing.*;
 
 class Main {
@@ -13,15 +14,25 @@ class Main {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
 
+        startScreen(panel, gbc);
+
+        frame.add(panel);
+        panel.setBackground(Colors.BASE);
+
+        frame.setVisible(true);
+    }
+
+    private static void quiz(JPanel panel, GridBagConstraints gbc) {
         JLabel questionLabel = new JLabel("QUESTION GOES HERE");
+        questionLabel.setForeground(Colors.TEXT);
 
         JTextField answerField = new JTextField(10);
-        JButton markButton = new JButton("Check");
 
-        // ensure the LAF paints the custom background color
+        JButton markButton = new JButton("Check");
         markButton.setBackground(Colors.MAUVE);
         markButton.setOpaque(true);
         markButton.setBorderPainted(false);
+        markButton.setFocusPainted(false);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -34,10 +45,6 @@ class Main {
         gbc.gridx = 1;
         panel.add(markButton, gbc);
 
-        frame.add(panel);
-        panel.setBackground(Colors.BASE);
-
-        frame.setVisible(true);
     }
 
     private static JFrame initFrame(String windowTitle) {
@@ -48,5 +55,31 @@ class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         return frame;
+    }
+
+    private static void startScreen(JPanel panel, GridBagConstraints gbc) {
+        JLabel label = new JLabel("Bible Quiz");
+        JButton button = new JButton("START!");
+
+        label.setForeground(Colors.TEXT);
+
+        button.setBackground(Colors.MAUVE);
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+
+
+        button.addActionListener(e -> {
+            panel.removeAll();
+            quiz(panel, gbc);
+            panel.revalidate();
+            panel.repaint();
+        });
+
+        panel.add(label, gbc);
+
+        gbc.gridy = 1;
+        panel.add(button, gbc);
+
     }
 }
